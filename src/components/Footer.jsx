d46346bdd5f7b775ba378904dbe0b91441fa2f9f
Vichu-007 <111241582+Vichu-007@users.noreply.github.com>
2025-08-15 14:38:49 +0200

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => (
+  <footer className="bg-gray-800 text-white py-6 px-4 text-center">
+    <p>
+      &copy; 2024{" "}
+      <a
+        href=""
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-400 hover:underline"
+      ></a>
+    </p>
+  </footer>
+);
+
+export default Footer;
